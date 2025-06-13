cfb79561160cfedfcc9338daede6c6bb600ac8e3
Noah <qutetutorials@gmail.com>
2025-06-13 22:24:33 +0200

    Update youtube-embed.js

--- js/youtube-embed.js
+++ js/youtube-embed.js
@@ -25,8 +25,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 const videoId = data.items[0].id.videoId;
                 if (videoId) {
                     const iframe = document.createElement("iframe");
-                    iframe.width = "1280px";
-                    iframe.height = "720px";
+                    iframe.width = "960px";
+                    iframe.height = "540px";
                     iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=0&controls=1&rel=0`;
                     iframe.title = "YouTube Video Player";
                     iframe.frameBorder = "0";
