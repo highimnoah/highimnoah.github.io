cbe7c3d8b6c897116016cd50e25061506bce76e7
Noah <qutetutorials@gmail.com>
2025-07-28 13:41:05 +0200

    Update page.js

--- src/app/archive/page.js
+++ src/app/archive/page.js
@@ -24,10 +24,10 @@ export default function Archive() {
                         delay={0.4}
                     >
                         <a
-                            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-12 px-4 w-20 sm:w-25 sm:scale-100 scale-120 sm:m-0 sm:mb-2 m-1"
-                            href="/api/download/Don't Be Long.mp3"
+                            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-12 px-4 w-25 sm:w-30 sm:scale-100 scale-120 sm:m-0 sm:mb-2 m-1"
+                            href="http://192.168.178.67:9002/api/raw/fileserver/chalice%202018%20452%20v1.1%20laced%20open%2087bpm.wav?"
                         >
-                            Test DL
+                            Laced ⬇️
                         </a>
                     </AnimatedContent>
                 </div>
